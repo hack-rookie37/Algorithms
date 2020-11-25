594b3829c672be575677ff808d12c6f21c091814
hack-rookie37 <dev.rookie37@gmail.com>
2020-11-25 19:14:14 +0900

    solved findTraversal by Pre-order

--- YONSEI/202002_ds_assignment5/problem1.py
+++ YONSEI/202002_ds_assignment5/problem1.py
@@ -2,10 +2,33 @@
 Student ID:
 NAME :
 '''
+from BinarySearchTree import *
 
-def findTraversal(tree):
-    ## CODE HERE ##
-    pass
+
+def findTraversal(tree):  # DFS, Pre-order(NLR)
+    if isinstance(tree, BinarySearchTree):  # tree가 BST의 instance이면
+        tree = tree.root  # tree를 tree의 root node로 assign.  
+        # from:  type(tree) == BinarySearchTree.BinarySearchTree
+        # to:  type(tree) == BinarySerachTree.TreeNode
+    
+    '''
+    # Pre-order 확인용 key 출력 code
+    if isinstance(tree, TreeNode):
+        print(tree.key)
+    '''
+    
+    if tree == None:  # 비어있는 자식이면 payload = 0으로 return
+        return 0
+    payload = tree.payload  # payload -> TreeNode.payload
+    left, right = 0, 0 # left, right 
+    left += findTraversal(tree.left_child) # sum of leftchilds' payloads
+    right += findTraversal(tree.right_child) # sum of rightchilds' payloads
+    if left > right: # compare
+        payload += left # assign
+    else:
+        payload += right # assing
+    return payload # return
+
 
 def findGreedy(tree):
     ## CODE HERE ##
