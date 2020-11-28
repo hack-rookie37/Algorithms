dcef729fdb82a7cd5f9a56ee3d751c5ce88a8e01
hack-rookie37 <dev.rookie37@gmail.com>
2020-11-28 18:37:29 +0900

    adjust indentation

--- YONSEI/202002_ds_assignment5/problem1.py
+++ YONSEI/202002_ds_assignment5/problem1.py
@@ -16,10 +16,10 @@ def findTraversal(tree):  # DFS, Pre-order(NLR)
     if isinstance(tree, TreeNode):
         print(tree.key)
     '''
-    if tree == None: # Basecase. (Just in case)
+    if tree == None:  # Basecase. (Just in case)
         return 0
-    
-    #tree is instance of TreeNode
+
+    # tree is instance of TreeNode
     payload = tree.payload
     left, right = 0, 0  # sum of payloads of left children, right children
     if tree.has_left_child():
@@ -29,29 +29,29 @@ def findTraversal(tree):  # DFS, Pre-order(NLR)
     if left > right:  # compare and assign bigger one
         payload += left
     else:
-        payload += right  
+        payload += right
     return payload  # return final payload
 
 
 def findGreedy(tree):
-    if isinstance(tree, BinarySearchTree): # tree가 BST의 instance이면
-        tree = tree.root # tree를 tree의 root node로 assign.
+    if isinstance(tree, BinarySearchTree):  # tree가 BST의 instance이면
+        tree = tree.root  # tree를 tree의 root node로 assign.
         # from:  type(tree) == BinarySearchTree.BinarySearchTree
         # to:  type(tree) == BinarySerachTree.TreeNode
-    
-    if tree == None: # Basecase. (Just in case)
+
+    if tree == None:  # Basecase. (Just in case)
         return 0
-    
-    payload = tree.payload # payload -> TreeNode.payload
-    if not tree.has_any_children(): # Both childs are null
-        return payload # return payload itself
-    elif not tree.has_left_child(): # left is null
-        payload += findGreedy(tree.right_child) # goto right
-    elif not tree.has_right_child(): # right is null
-        payload += findGreedy(tree.left_child) # goto left
+
+    payload = tree.payload  # payload -> TreeNode.payload
+    if not tree.has_any_children():  # Both childs are null
+        return payload  # return payload itself
+    elif not tree.has_left_child():  # left is null
+        payload += findGreedy(tree.right_child)  # goto right
+    elif not tree.has_right_child():  # right is null
+        payload += findGreedy(tree.left_child)  # goto left
     else:
-        if tree.left_child.payload > tree.right_child.payload: # compare and decide where to go find
-            payload += findGreedy(tree.left_child) # goto left
+        if tree.left_child.payload > tree.right_child.payload:  # compare and decide where to go find
+            payload += findGreedy(tree.left_child)  # goto left
         else:
-            payload += findGreedy(tree.right_child) # goto right
+            payload += findGreedy(tree.right_child)  # goto right
     return payload
