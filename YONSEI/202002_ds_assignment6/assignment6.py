76a973bb1d8e36e87e63ff3741c6491d470b4017
hack-rookie37 <dev.rookie37@gmail.com>
2020-12-09 14:16:32 +0900

    autopep8

--- YONSEI/202002_ds_assignment6/assignment6.py
+++ YONSEI/202002_ds_assignment6/assignment6.py
@@ -1,22 +1,25 @@
-import math, sys
+import math
+import sys
 
 ################ DO NOT modify this code ######################
+
+
 class PriorityQueue:
     def __init__(self):
-        self.heapArray = [(0,0)]
+        self.heapArray = [(0, 0)]
         self.currentSize = 0
 
-    def buildHeap(self,alist):
+    def buildHeap(self, alist):
         self.currentSize = len(alist)
-        self.heapArray = [(0,0)]
+        self.heapArray = [(0, 0)]
         for i in alist:
             self.heapArray.append(i)
-        i = len(alist) // 2            
+        i = len(alist) // 2
         while (i > 0):
             self.percDown(i)
             i = i - 1
-                        
-    def percDown(self,i):
+
+    def percDown(self, i):
         while (i * 2) <= self.currentSize:
             mc = self.minChild(i)
             if self.heapArray[i][0] > self.heapArray[mc][0]:
@@ -24,8 +27,8 @@ class PriorityQueue:
                 self.heapArray[i] = self.heapArray[mc]
                 self.heapArray[mc] = tmp
             i = mc
-                
-    def minChild(self,i):
+
+    def minChild(self, i):
         if i*2 > self.currentSize:
             return -1
         else:
@@ -37,15 +40,15 @@ class PriorityQueue:
                 else:
                     return i*2+1
 
-    def percUp(self,i):
+    def percUp(self, i):
         while i // 2 > 0:
             if self.heapArray[i][0] < self.heapArray[i//2][0]:
                 tmp = self.heapArray[i//2]
                 self.heapArray[i//2] = self.heapArray[i]
                 self.heapArray[i] = tmp
             i = i//2
- 
-    def add(self,k):
+
+    def add(self, k):
         self.heapArray.append(k)
         self.currentSize = self.currentSize + 1
         self.percUp(self.currentSize)
@@ -57,14 +60,14 @@ class PriorityQueue:
         self.heapArray.pop()
         self.percDown(1)
         return retval
-        
+
     def isEmpty(self):
         if self.currentSize == 0:
             return True
         else:
             return False
 
-    def decreaseKey(self,val,amt):
+    def decreaseKey(self, val, amt):
         done = False
         i = 1
         myKey = 0
@@ -75,17 +78,18 @@ class PriorityQueue:
             else:
                 i = i + 1
         if myKey > 0:
-            self.heapArray[myKey] = (amt,self.heapArray[myKey][1])
+            self.heapArray[myKey] = (amt, self.heapArray[myKey][1])
             self.percUp(myKey)
-            
-    def __contains__(self,vtx):
+
+    def __contains__(self, vtx):
         for pair in self.heapArray:
             if pair[1] == vtx:
                 return True
         return False
 
+
 class Vertex:
-    def __init__(self,key):
+    def __init__(self, key):
         self.id = key
         self.connectedTo = {}
         self.color = 'white'
@@ -93,73 +97,74 @@ class Vertex:
         self.pred = None
         self.disc = 0
         self.fin = 0
-    
-    def addNeighbor(self,nbr,weight=0):
+
+    def addNeighbor(self, nbr, weight=0):
         self.connectedTo[nbr] = weight
-        
-    def setColor(self,color):
+
+    def setColor(self, color):
         self.color = color
-        
-    def setDistance(self,d):
+
+    def setDistance(self, d):
         self.dist = d
 
-    def setPred(self,p):
+    def setPred(self, p):
         self.pred = p
 
-    def setDiscovery(self,dtime):
+    def setDiscovery(self, dtime):
         self.disc = dtime
-        
-    def setFinish(self,ftime):
+
+    def setFinish(self, ftime):
         self.fin = ftime
-        
+
     def getFinish(self):
         return self.fin
-        
+
     def getDiscovery(self):
         return self.disc
-        
+
     def getPred(self):
         return self.pred
-        
+
     def getDistance(self):
         return self.dist
-        
+
     def getColor(self):
         return self.color
-    
+
     def getConnections(self):
         return self.connectedTo.keys()
 
     def getId(self):
         return self.id
-    
-    def getWeight(self,nbr):
+
+    def getWeight(self, nbr):
         return self.connectedTo[nbr]
 
     def __str__(self):
-        return str(self.id) + ' connectedTo: ' + str([x.id for x in self.connectedTo])    
+        return str(self.id) + ' connectedTo: ' + str([x.id for x in self.connectedTo])
+
 
 class Graph:
     def __init__(self):
         self.vertList = {}
         self.numVertices = 0
 
-    def addVertex(self,key):
+    def addVertex(self, key):
         self.numVertices = self.numVertices + 1
         newVertex = Vertex(key)
         self.vertList[key] = newVertex
         return newVertex
 
-    def getVertex(self,n):
+    def getVertex(self, n):
         if n in self.vertList:
             return self.vertList[n]
         else:
             return None
 
-    def __contains__(self,n):
+    def __contains__(self, n):
         return n in self.vertList
 
-    def addEdge(self,f,t,weight=0):
+    def addEdge(self, f, t, weight=0):
         if f not in self.vertList:
             nv = self.addVertex(f)
         if t not in self.vertList:
@@ -171,137 +176,57 @@ class Graph:
 
     def __iter__(self):
         return iter(self.vertList.values())
-    
+
 ##################################################################
 
 ######## Problem 1 #########
+
+
 def kruskal(graph):
     pq = PriorityQueue()
     kruskal_graph = Graph()
-    parent = {(k,k) for k in graph}
-    
+    parent = {(k, k) for k in graph}
+
     pq.buildHeap(
         []
     )
-        
-    #print(pq.heapArray)
+
+    # print(pq.heapArray)
     return graph
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
 
 def prim(graph, start_vertex):
     pq = PriorityQueue()
     prim_graph = Graph()
+    for v in graph:
+        v.setDistance(sys.maxsize)
+        v.setPred(None)
     graph.vertList[start_vertex].setDistance(0)
-    pq.buildHeap([(v.getDistance(),v) for v in graph])
+    pq.buildHeap([(v.getDistance(), v) for v in graph])
     while not pq.isEmpty():
         currentVert = pq.delMin()
         for nextVert in currentVert.getConnections():
-          newCost = currentVert.getWeight(nextVert)
-          if nextVert in pq and newCost < nextVert.getDistance():
-              prim_graph.addEdge(currentVert.getId(), nextVert.getId(), newCost)
-              prim_graph.addEdge(nextVert.getId(), currentVert.getId(), newCost)
-              nextVert.setPred(currentVert)
-              nextVert.setDistance(newCost)
-              pq.decreaseKey(nextVert,newCost)
+            newCost = currentVert.getWeight(nextVert)
+            if nextVert in pq and newCost < nextVert.getDistance():
+
+                nextVert.setPred(currentVert)
+                nextVert.setDistance(newCost)
+                pq.decreaseKey(nextVert, newCost)
+
     return prim_graph
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
 
 ######## Problem 2 #########
 class PointVertex:
-    
-    
-    pass
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-class PointGraph:
-    
-    
-    pass
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-def bellman_ford(graph, start_vertex):
-    
-    
-    
-    pass
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
 
+    pass
+
+
+class PointGraph:
+
+    pass
+
+
+def bellman_ford(graph, start_vertex):
+
+    pass
